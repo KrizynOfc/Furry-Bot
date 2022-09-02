d1e747fc9a3d6c619a5cf7145b2b56b69df7e174
Bisnisokteto <110271297+Bisnisokteto@users.noreply.github.com>
2022-09-02 18:39:17 +0700

    Yy
    Yy

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['16199961931', 'Ziv San', true], 
-['6282179137771', 'HAORI IQ-MD', true] 
+['16199961931', 'Nainn', true], 
+['6288223674569', 'Meldaa - MD', true] 
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
